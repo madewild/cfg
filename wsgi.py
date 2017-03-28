70615e547ac7027973f176b5d439205ef9e862f3
madewild <madewild@ulb.ac.be>
2017-03-28 06:22:58 +0200

    title

--- wsgi.py
+++ wsgi.py
@@ -87,7 +87,7 @@ def application(environ, start_response):
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>Welcome to OpenShift</title>
+  <title>TP CFG</title>
 <style>
 
 /*!
