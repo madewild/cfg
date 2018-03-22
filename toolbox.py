5833219b4042e61d9e0e97481f181af548cf4d59
madewild <madewild@ulb.ac.be>
2018-03-22 09:49:11 +0100

    add templates

---
+++ toolbox.py
@@ -0,0 +1,42 @@
+"""Functions for CFG testing"""
+
+import nltk
+
+def test_cfg(string):
+    '''Compute score based on CFG input'''
+    pos = open('pos.txt').readlines()
+    pos = [line.strip() for line in pos]
+
+    neg = open('neg.txt').readlines()
+    neg = [line.strip() for line in neg]
+
+    lowstring = string.lower()
+    gram = nltk.CFG.fromstring(lowstring)
+    parser = nltk.ChartParser(gram)
+    p = 0
+    n = 0
+    fneg = []
+    fpos = []
+    for text in pos:
+        tokens = nltk.tokenize.wordpunct_tokenize(text.lower())
+        try:
+            trees = parser.parse(tokens)
+        except ValueError:
+            trees = []
+        if len(list(trees)) > 0:
+            p += 1
+        else:
+            fneg.append(text)
+    for text in neg:
+        tokens = nltk.tokenize.wordpunct_tokenize(text.lower())
+        try:
+            trees = parser.parse(tokens)
+        except ValueError:
+            trees = []
+        for tree in trees:
+            if tree:
+                n += 1
+                fpos.append(text)
+                break
+    score = max(0, p-n)
+    return score, p, n, fneg, fpos
--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,24 @@
 """Main web server logic"""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from toolbox import test_cfg
 application = Flask(__name__)
 
-@application.route("/")
+@application.route("/", methods=['GET', 'POST'])
 def form():
     return render_template('form.html')
 
 @application.route("/correction", methods=['GET', 'POST'])
 def corr():
-    return render_template('corr.html')
+    data = request.form
+    gram = data['cfg']
+    try:
+        score, p, n, fneg, fpos, = test_cfg(gram)
+        correct = 'phrase correcte' if p == 1 else 'phrases correctes'
+        incorrect = 'phrase incorrecte' if n == 1 else 'phrases incorrectes'
+        return render_template('corr.html', score=str(score), p=str(p), n=str(n), correct=correct, incorrect=incorrect, fneg=fneg, fpos=fpos)
+    except ValueError:
+        return render_template('wrong.html', gram=gram)
 
 if __name__ == "__main__":
     application.run()
